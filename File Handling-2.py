efbf015670230818671ea1542d9a9fd35b8b0d93
Nusrat-2808 <hmseeyam02092004@gmail.com>
2025-08-09 22:09:33 +0600

    Delete File Handling-2.py

--- File Handling-2.py
+++
@@ -1,9 +0,0 @@
-file=open('codingal.txt','r')
-Counter=0
-Content=file.read()
-CoList=Content.split("\n")
-for i in CoList:
-    if i:
-        Counter+=1
-print("This is the number of lines in the file_")
-print(Counter)
